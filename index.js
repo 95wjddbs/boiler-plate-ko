4f7f9c9f56659f6683987b68a0dbb8ada0a2a1b6
95wjddbs <66461799+95wjddbs@users.noreply.github.com>
2022-08-12 16:11:38 +0900

    0812 최종 수정

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const { User } = require("./models/User");
+const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 
@@ -10,8 +11,8 @@ const config = require("./config/key");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json 을 분석해서 가져옴
-// test
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -31,10 +32,40 @@ app.post("/register", (req, res) => {
 
   const user = new User(req.body);
 
-  user.save((err, doc) => {
+  user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 이메일을 DB에서 있는지 찾음.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    //요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //토큰을 저장함 어디에? 쿠키, 로컬스토리지 등
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
